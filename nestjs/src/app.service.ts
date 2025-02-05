37d26f5e63c72ce35b0553e57994d9f3ccc919b1
Alan Ramalho <ramalho.sit@gmail.com>
2025-02-05 00:00:14 -0300

    finalizando estudos

--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+let count = 0;
+
 @Injectable()
 export class AppService {
   async getHello() {
+    
     try {
-      const request = await fetch('https://cep.awesomeapi.com.br/json/20521100');
+      const request = await fetch('https://cep.awesomeapi.com.br/json/70070080');
       const data = await request.json();
-      return data;
+      count = count + 1;
+      return {
+        count,
+        data,
+      };
     } catch (error) {
       console.log(error)
       return 'error!';
