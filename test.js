d4e4379c1d3e52c1e5214e96c1c04ad349ef7bac
Alan Ramalho <ramalho.sit@gmail.com>
2025-02-19 18:16:48 -0300

    ajustes e testes

--- test.js
+++ test.js
@@ -3,18 +3,18 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 20 },  // 10 usuários em 30s
-    { duration: '1m', target: 50 },   // 50 usuários por 1 min
+    { duration: '30s', target: 30 },  // 10 usuários em 30s
+    { duration: '30', target: 500 },   // 50 usuários por 1 min
     { duration: '30s', target: 0 },   // Redução gradual para 0
   ],
 };
 
 export default function () {
-  let res = http.get('http://10.2.1.125:32000?cep=20521100', { timeout: '1000s' });
+  let res = http.get('https://clubebetta.thinklife.com.br/banners', { timeout: '1000s' });
 
   check(res, {
     'status é 200': (r) => r.status === 200,
-    'tempo de resposta < 500ms': (r) => r.timings.duration < 500,
+    'tempo de resposta < 500ms': (r) => r.timings.duration < 5200,
   });
   sleep(1);
 }
